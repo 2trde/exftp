6bc3b9a7402f2e8d4446324ba9ce09184bbd7ecf
Marcus Lankenau <marcus.lankenau@2trde.com>
2019-08-28 10:01:06 +0200

    alow to set port for sftp in options

--- lib/ex_ftp.ex
+++ lib/ex_ftp.ex
@@ -4,12 +4,16 @@ defmodule ExFtp do
   Returns the pid that has to be passed to execute commands on that connection
   """
   def open(host, user, password, options \\ [])
-  def open(host, user, password, [mode: :sftp]) do
+  def open(host, user, password, options) do
+    open(host, user, password, Keyword.get(options, :mode), options)
+  end
+  def open(host, user, password, :sftp, options) do
+    port = Keyword.get(options, :port, 22)
     :ok = :ssh.start()
-    {:ok, channel_pid, connection} = :ssh_sftp.start_channel(s_to_l(host), [user: s_to_l(user), password: s_to_l(password), silently_accept_hosts: true])
+    {:ok, channel_pid, connection} = :ssh_sftp.start_channel(s_to_l(host), port, [user: s_to_l(user), password: s_to_l(password), silently_accept_hosts: true])
     {:sftp, channel_pid, connection}
   end
-  def open(host, user, password, options) do
+  def open(host, user, password, _, options) do
     :inets.start
     {:ok, pid} = :inets.start(:ftpc, Keyword.merge(options, [host: host |> String.to_charlist]))
     :ok = :ftp.user(pid, user |> String.to_charlist, password |> String.to_charlist)
